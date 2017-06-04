ed37e1a9dea2c9df0e5ea99c8288492df9f526f3
Charles Severance <csev@umich.edu>
2017-06-03 21:33:23 -0400

    Tweaks

--- python/index.php
+++ python/index.php
@@ -1,4 +1,11 @@
 <h1>Python Classes</h1>
+<p>This is just a flat HTML file pointing to some directories.  It shows
+how Koseu can be mounted at many places and levels in a folder hierarchy
+and how HTML and LMS links can just be mixed as needed.
+</p>
+<p>
+Pay close attention to the URLs as you navigate around...
+</p>
 <ul>
 <li><p><a href="py4e">Python For Everybody</a></p></li>
 <li><p><a href="py4d">Data Mining for Everybody</a></p></li>
